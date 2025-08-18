481be4f64639263c32581cb73674ec2ae5a5fec8
Nuno Martins <nmartins@redhat.com>
2025-08-18 11:50:21 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -164,20 +164,28 @@ tee /tmp/setup.yml << EOF
     aws_default_region: "{{ lookup('env', 'AWS_DEFAULT_REGION') | default('AWS_DEFAULT_REGION_NOT_FOUND', true) }}"
     quay_username: "{{ lookup('env', 'QUAY_USERNAME') | default('QUAY_USERNAME_NOT_FOUND', true) }}"
     quay_password: "{{ lookup('env', 'QUAY_PASSWORD') | default('QUAY_PASSWORD_NOT_FOUND', true) }}"
+    azure_subscription: "{{ lookup('env', 'AZURE_SUBSCRIPTION') | default('AZURE_SUBSCRIPTION_NOT_FOUND', true) }}"
+    azure_tenant: "{{ lookup('env', 'AZURE_TENANT') | default('AZURE_TENANT_NOT_FOUND', true) }}"
+    azure_client_id: "{{ lookup('env', 'AZURE_CLIENT_ID') | default('AZURE_CLIENT_ID_NOT_FOUND', true) }}"
+    azure_password: "{{ lookup('env', 'AZURE_PASSWORD') | default('AZURE_PASSWORD_NOT_FOUND', true) }}"
+    azure_resourcegroup: "{{ lookup('env', 'AZURE_RESOURCEGROUP') | default('AZURE_RESOURCEGROUP_NOT_FOUND', true) }}"
 
   tasks:
-    - name: Add AWS credential
+  
+    - name: Add Azure credential
       ansible.controller.credential:
-        name: 'AWS Credential'
+        name: 'Azure Credential'
         organization: Default
-        credential_type: "Amazon Web Services"
+        credential_type: "Microsoft Azure Resource Manager"
         controller_host: "https://localhost"
         controller_username: admin
         controller_password: ansible123!
         validate_certs: false
         inputs:
-          username: "{{ aws_access_key }}"
-          password: "{{ aws_secret_key }}"
+          subscription: "{{ azure_subscription }}"
+          secret: "{{ azure_password }}"
+          client: "{{ azure_client_id }}"
+          tenant: "{{ azure_tenant }}"
 
     # - name: Remove Demo Inventory
     #   ansible.controller.inventory:

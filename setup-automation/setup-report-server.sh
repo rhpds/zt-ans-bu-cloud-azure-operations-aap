e8adcacd03139641e1acd5c04c479408d2c6ec6c
Nuno Martins <nmartins@redhat.com>
2025-10-03 16:05:18 +0200

    Update setup-report-server.sh

--- setup-automation/setup-report-server.sh
+++ setup-automation/setup-report-server.sh
@@ -5,4 +5,4 @@ rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
 
 subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY}
 setenforce 0
-
+sudo useradd rheluser && echo "rheluser:RedHatAnsible123!" | sudo chpasswd
